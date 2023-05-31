03876678fbf8b07a5db14e7a8b042762120c34c5
Ranielli Montagna <raniellimontagna@hotmail.com>
2023-05-31 19:32:57 -0300

    refactor: :recycle: implementa vitest globals e ajusta path aliases

--- src/domain/entities/instructor.ts
+++ src/domain/entities/instructor.ts
@@ -1,6 +1,5 @@
-import { Entity } from '../../core/entities/entity'
-import { UniqueEntityID } from '../../core/entities/unique-entity-id'
-
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { Entity } from '@/core/entities/entity'
 interface InstructorProps {
   name: string
 }
--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,9 +1,11 @@
 import dayjs from 'dayjs'
 
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { Entity } from '@/core/entities/entity'
+import { Optional } from '@/core/types/optional'
+
 import { Slug } from './value-objects/slug'
-import { Entity } from '../../core/entities/entity'
-import { UniqueEntityID } from '../../core/entities/unique-entity-id'
-import { Optional } from '../../core/types/optional'
+
 interface QuestionProps {
   authorId: UniqueEntityID
   bestAnswerId?: UniqueEntityID
